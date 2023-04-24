fd816bcde6f7ad3a3135b607a91d714475caae8e
Jaine Beling <jaine.mazuim2016@gmail.com>
2023-04-24 15:16:02 -0300

    ajustes

--- forca.py
+++ forca.py
@@ -2,6 +2,24 @@ def jogar():
   print("********************************")
   print("***Bem Vindo ao Jogo da Forca***")
   print("********************************")
+  palavra_secreta = "banana"
+  letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
+  enforcou = False
+  acertou = False
+  print(letras_acertadas)
+
+  while(not enforcou and not acertou):
+
+    chute = input("Qual letra? ")
+    chute = chute.strip()
+
+    index = 0
+    for letra in palavra_secreta:
+      if(chute.upper() == letra.upper()):
+        letras_acertadas[index] = letra
+      index = index + 1
+    print(letras_acertadas)
 
   print("Fim do Jogo")
 
