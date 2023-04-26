a5cb5add5f8914c54066e05bbeaf8fe96ffc9dd2
Jaine Beling <jaine.mazuim2016@gmail.com>
2023-04-25 21:01:50 -0300

    ajustes

--- forca.py
+++ forca.py
@@ -1,41 +1,50 @@
 import random
 
-  
-
 def jogar():
   imprime_mensagem_abertura()
   palavra_secreta = carrega_palavra_secreta()
+
   letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+  print(letras_acertadas)
 
   enforcou = False
   acertou = False
   erros = 0
 
-  print(letras_acertadas)
-
   while(not enforcou and not acertou):
 
-    chute = input("Qual letra? ")
-    chute = chute.strip().upper()
+    chute = pede_chute()
 
     if (chute in palavra_secreta):
-      index = 0
-      for letra in palavra_secreta:
-        if(chute == letra):
-          letras_acertadas[index] = letra
-        index += 1
+      marca_chute_correto(chute, letras_acertadas, palavra_secreta)
     else:
       erros =+ 1
     enforcou = erros == 6
     acertou = "_" not in letras_acertadas
     print(letras_acertadas)
   if(acertou):
-    print("Você Ganhou!")
+    imprime_mensagem_vencedor()
   else:
-    print("Você perdeu!")
-  print("Fim do Jogo")
+    imprime_mensagem_perdedor()
 
 
+def imprime_mensagem_vencedor():
+  print("Você Ganhou!")
+
+def imprime_mensagem_perdedor():
+  print("Você perdeu!")
+
+def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if(chute == letra):
+          letras_acertadas[index] = letra
+        index += 1
+
+def pede_chute():
+  chute = input("Qual letra? ")
+  chute = chute.strip().upper()
+  return chute
 
 def inicializa_letras_acertadas(palavra):
   return ["_" for letra in palavra]
